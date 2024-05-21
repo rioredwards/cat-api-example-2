edcc65a7084cf7e61ab87b20feed7a5c749f6309
Rio Edwards <rioredwards@gmail.com>
2024-05-21 12:24:17 -0700

    Fetching a cat from button press

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
 const catContainer = document.getElementById("catContainer");
+const fetchCatBtn = document.getElementById("fetchCat");
 
-fetch(
-  "https://api.thecatapi.com/v1/images/search?limit=1&has_breeds=1&api_key=live_YNdBDkb6hJuKn2sTeEUeI2J2p14xqL77loXkCwJJVr284EgKEM1nSrVDrdrpCZKb"
-)
-  .then((res) => {
-    if (!res.ok) {
-      throw new Error("Invalid Request");
-    }
-    return res.json();
-  })
-  .then((data) => {
-    console.log(data);
-    const catImgUrl = data[0].url;
-    const catImg = document.createElement("img");
-    catImg.src = catImgUrl;
+fetchCatBtn.addEventListener("click", () => {
+  fetch(
+    "https://api.thecatapi.com/v1/images/search?limit=1&has_breeds=1&api_key=live_YNdBDkb6hJuKn2sTeEUeI2J2p14xqL77loXkCwJJVr284EgKEM1nSrVDrdrpCZKb"
+  )
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Invalid Request");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      // Clear out previous cat
+      catContainer.innerHTML = "";
 
-    console.log(data[0].breeds[0].description);
-    const breedName = data[0].breeds[0].name;
-    const catBreedTitle = document.createElement("h2");
-    catBreedTitle.innerText = breedName;
+      const catImgUrl = data[0].url;
+      const catImg = document.createElement("img");
+      catImg.src = catImgUrl;
 
-    const breedDescription = document.createElement("p");
-    breedDescription.innerText = data[0].breeds[0].description;
+      const breedName = data[0].breeds[0].name;
+      const catBreedTitle = document.createElement("h2");
+      catBreedTitle.innerText = breedName;
 
-    catContainer.appendChild(catBreedTitle);
-    catContainer.appendChild(breedDescription);
-    catContainer.appendChild(catImg);
-  })
-  .catch((err) => {
-    console.warn(err);
-  });
+      const breedDescription = document.createElement("p");
+      breedDescription.innerText = data[0].breeds[0].description;
+
+      catContainer.appendChild(catBreedTitle);
+      catContainer.appendChild(breedDescription);
+      catContainer.appendChild(catImg);
+    })
+    .catch((err) => {
+      console.warn(err);
+    });
+});
